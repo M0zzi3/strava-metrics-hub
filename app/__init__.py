0efecf7c464472dd1dc2ebdb2e765353139e4ffe
M0zzi3 <not.mozzi3@gmail.com>
2025-12-18 11:54:49 +0100

    fix(db): import models in app context to fix missing schema

--- app/__init__.py
+++ app/__init__.py
@@ -8,23 +8,22 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    # Secure key
+    # 1. Config
     app.config['SECRET_KEY'] = 'dev'
-
-    # Database connection (reads from Docker env)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    # Register blueprints
+    # 2. Register Blueprints
     from .routes import main
     app.register_blueprint(main)
 
-    from .models import Athlete, Activity
-
-    # Create tables
+    # 3. Create Tables (The Fix is here)
     with app.app_context():
+        # IMPORT MODELS HERE so SQLAlchemy 'sees' them before creating tables
+        from . import models
+
         db.create_all()
 
     return app
